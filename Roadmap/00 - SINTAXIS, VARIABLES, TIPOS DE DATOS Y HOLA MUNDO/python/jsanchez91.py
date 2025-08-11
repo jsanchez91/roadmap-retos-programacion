cc160a496005446fe1a101af82df33ff0065dfdc
Jose Sanchez <josesanxez@gmail.com>
2025-08-11 12:34:57 +0200

    Ejercicio 00 solución de jsanchez91

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/jsanchez91.py
@@ -0,0 +1,20 @@
+# https://python.org/
+
+# Comentario de una línea
+
+""" 
+Comentario 
+de varias 
+líneas
+"""
+
+my_variable = "Hola, mundo!"
+my_variable = "Otro valor"  
+
+
+string_variable = "Cadena de texto"
+number_variable = 1234 
+double_variable = .1234
+boolean_variable = True
+
+print("Hola mundo!")
